d7dc8971fad34fcb284b784df5903f39c722601e
Rakeshm31 <maddinenirakesh6@gmail.com>
2025-07-12 10:06:27 +0530

    Initial commit

---
+++ src/services/api.js
@@ -0,0 +1,14 @@
+const API="39f200c8d416f696c7828d8eba44a7fd";
+const BASE_URL = "https://api.themoviedb.org/3";
+
+export const getPopularMovies = async () => {
+    const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API}`);
+    const data = await response.json();
+    return data.results;
+}
+
+export const searchMovies = async (query) => {
+    const response = await fetch(`${BASE_URL}/search/movie?api_key=${API}&query=${encodeURIComponent(query)}`);
+    const data = await response.json();
+    return data.results;
+}
